85207e62a3578ab0195d5404989b744eda9c0d96
galean <galean.pal@gmail.com>
2026-02-24 17:43:20 +0200

    feat: Added manual feature flag fetching functionality

--- Sources/CoreIntegrations/Protocols/PublicProtocols/Analytics/CoreAnalyzableUserProperty.swift
+++ Sources/CoreIntegrations/Protocols/PublicProtocols/Analytics/CoreAnalyzableUserProperty.swift
@@ -5,7 +5,51 @@ import AnalyticsIntegration
 #endif
 
 public protocol CoreAnalyzableUserProperty: CaseIterable, AmplitudeAnalyzableUserProperty {
-//    static var att_status: Self { get }
-//    static var store_country: Self { get }
-//    static var subscription_type: Self { get }
+    
+}
+
+extension CoreAnalyzableUserProperty {
+    static func identify(_ userProperties: [String: Any], fetchFlags: Bool, numberOfTimes: Int = 1, completion: (() -> Void)?) {
+        identify(userProperties)
+        
+        guard fetchFlags else {
+            completion?()
+            return
+        }
+        
+        let group = DispatchGroup()
+        
+        for _ in 0..<numberOfTimes {
+            group.enter()
+            CoreManager.internalShared.remoteConfigManager?.updateRemoteConfig(userProperties) {
+                group.leave()
+            }
+        }
+        
+        group.notify(queue: .global()) {
+            completion?()
+        }
+    }
+    
+    func identify(parameter: String, fetchFlags: Bool, numberOfTimes: Int = 1, completion: (() -> Void)?) {
+        identify(parameter: parameter)
+        
+        guard fetchFlags else {
+            completion?()
+            return
+        }
+        
+        let group = DispatchGroup()
+
+        for _ in 0..<numberOfTimes {
+            group.enter()
+            CoreManager.internalShared.remoteConfigManager?.updateRemoteConfig([key: parameter]) {
+                group.leave()
+            }
+        }
+        
+        group.notify(queue: .global()) {
+            completion?()
+        }
+    }
 }
